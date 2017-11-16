bec7151ac69e2951b6108afcf8ab9773e9ce7b7f
yo7 <bloom6445@gmail.com>
2017-11-17 08:13:49 +0900

    p6. Prepare Dollar class

---
+++ chapter1/src/main/java/com/github/yo7/money/Dollar.java
@@ -0,0 +1,11 @@
+package com.github.yo7.money;
+
+public class Dollar {
+    public int amount;
+
+    Dollar(int amount) {
+    }
+
+    public void times(int i) {
+    }
+}
---
+++ chapter1/src/test/java/com/github/yo7/money/MoneyTest.java
@@ -0,0 +1,13 @@
+package com.github.yo7.money;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}

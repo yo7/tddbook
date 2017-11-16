282016db511aad92b7eca2acb0f570093f1061d7
yo7 <bloom6445@gmail.com>
2017-11-17 08:33:07 +0900

    p15. make times return Dollar object

--- chapter1/src/test/java/com/github/yo7/money/MoneyTest.java
+++ chapter1/src/test/java/com/github/yo7/money/MoneyTest.java
@@ -9,7 +9,7 @@ public class MoneyTest {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(10, product.amount);
-        product = five.times(5);
+        product = five.times(3);
         assertEquals(15, product.amount);
     }
 }

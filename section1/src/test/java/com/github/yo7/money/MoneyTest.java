b47c48fdd1ecf5fe2d88f3cd0b4fba3174b100ab
yo7 <bloom6445@gmail.com>
2017-11-19 19:27:28 +0900

    p50. Use factory method in spec

--- section1/src/test/java/com/github/yo7/money/MoneyTest.java
+++ section1/src/test/java/com/github/yo7/money/MoneyTest.java
@@ -1,23 +1,26 @@
 package com.github.yo7.money;
 
 import org.junit.Test;
+
+import java.awt.event.MouseEvent;
+
 import static org.junit.Assert.*;
 
 public class MoneyTest {
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar((15)), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(3).equals(new Dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(3).equals(Money.dollar(5)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(3).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(new Franc(5).equals(Money.dollar(5)));
     }
 
     @Test

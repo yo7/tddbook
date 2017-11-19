6fad5a8ddd7cb63cda06a2f4794e92e7fae04f9e
yo7 <bloom6445@gmail.com>
2017-11-19 19:33:48 +0900

    p51. Implement factory method for both Dollar and Franc

--- section1/src/main/java/com/github/yo7/money/Money.java
+++ section1/src/main/java/com/github/yo7/money/Money.java
@@ -12,7 +12,11 @@ abstract class Money {
                     && getClass().equals(money.getClass());
     }
 
-    static Dollar dollar(int amount) {
+    static Money dollar(int amount) {
         return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }

359c91f494dc1b7adcd42ca5fc4257b15adb5d3d
yo7 <bloom6445@gmail.com>
2017-11-22 08:28:49 +0900

    p93. Implements Bank reduce()

--- section1/src/main/java/com/github/yo7/money/Bank.java
+++ section1/src/main/java/com/github/yo7/money/Bank.java
@@ -2,6 +2,8 @@ package com.github.yo7.money;
 
 class Bank {
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
